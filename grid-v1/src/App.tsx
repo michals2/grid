72b5449e0e90f6bd48dc0186cb610970a424dec6
luke <lucasmichals@Macbooks-MacBook-Pro.local>
2020-04-18 18:12:35 -0700

    add styled components

--- grid-v1/src/App.tsx
+++ grid-v1/src/App.tsx
@@ -1,14 +1,46 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const StyledApp = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: grid;
+  grid-template-areas:
+    "launcher explorer viewer"
+    "footer footer footer";
+  grid-template-rows: auto 20px;
+  grid-template-columns: 50px 200px auto;
+`;
+
+const StyledLauncher = styled.div`
+  grid-area: launcher;
+`;
+const StyledExplorer = styled.div`
+  grid-area: explorer;
+`;
+const StyledViewer = styled.div`
+  grid-area: viewer;
+`;
+const StyledFooter = styled.div`
+  grid-area: footer;
+`;
+
+export default () => {
   return (
-    <div className="App">
-      <div className="launcher">launcher</div>
-      <div className="explorer">explorer</div>
-      <div className="viewer">viewer</div>
-      <div className="footer">footer</div>
-    </div>
+    <StyledApp className="App">
+      <GlobalStyle />
+      <StyledLauncher>launcher</StyledLauncher>
+      <StyledExplorer>explorer</StyledExplorer>
+      <StyledViewer>viewer</StyledViewer>
+      <StyledFooter>footer</StyledFooter>
+    </StyledApp>
   );
-}
-
-export default App;
+};
